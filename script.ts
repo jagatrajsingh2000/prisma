3930755edab9300a4941ad54c11855982bc1924a
jagatrajsingh2000 <jagatrajsingh2000@gmail.com>
2023-07-19 16:32:04 +0530

    updated apis

--- script.ts
+++ script.ts
@@ -1,10 +1,85 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
+// const prisma = new PrismaClient({log: ["query"]})
 
 async function main(){
-    const user = await prisma.user.findMany
+    // await prisma.user.deleteMany()
+
+    // const user = await prisma.user.update({
+    //     where: {
+    //         email:'jagat@gmail.com'
+           
+    //     },
+    //     data: {
+    //         email: "jagatraj@gmail.com",
+    //     }
+    // })
+    const user = await prisma.user.update({
+        where: {
+            email:'jagat@gmail.com'
+           
+        },
+        data: {
+            age:{
+                // multiply: 10
+                increment: 10
+            }
+        }
+    })
+
+    // const user = await prisma.user.findUnique({
+    //     where: {
+    //         age_name:{
+    //             age:16,
+    //             name:'jagat'
+    //         }
+    //     }
+    // })
+    // must have unique property to it 
+
+    // const user = await prisma.user.findFirst({
+    //     where: {
+    //         name:'jagat'
+    //     }
+    // })
+    // find first thing which  matches
+
+     // const user = await prisma.user.findMany({
+    //     where: {
+    //         name:'jagat'
+    //     }
+    // })
+    // find first thing which  matches 
+
+
+
+    // const user = await prisma.user.create({
+    //     data: {
+    //         name:'jagat',
+    //         email:'jagat@gmail.com',
+    //         age:16,
+    //         userPreference: {
+    //             create: {
+    //                 emailUpdates: true
+    //             },
+    //         },
+            
+    //     },
+    //     // include: {
+    //     //     userPreference: true
+    //     // }
+    //     select: {
+    //         name:true,
+    //         userPreference:{ select: {
+    //             id : true
+    //         }}
+    //     }
+    //     //  can use include and select together
+        
+    // })
     console.log(user)
 }
+
 
 main()
     .catch(e => {
